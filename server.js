e8c3d6b1c807e9e36a5f4234b840ecb8ea676526
jh-yng <joshyang@umd.edu>
2022-07-02 17:54:37 -0500

    commit on wp

---
+++ server.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+import express from 'express';
+// import apiRoutes from './routes/apiRoutes.js';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const staticFolder = 'client/public';
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(staticFolder));
+// app.use('/api', apiRoutes);
+
+async function bootServer() {
+  try {
+    // const mysql = await db.sequelizeDB;
+    // await mysql.sync();
+    app.listen(PORT, () => {
+      console.log(`Listening on: http//localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+bootServer();
